7344bea06ea74645edfe8613430046be8678c75c
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2022-08-09 14:10:11 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+let now = new Date();
+let currentDate = document.querySelector("#date");
+let months = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let year = now.getFullYear();
+currentDate.innerHTML = `${month}/${date}/${year}`;
+
+let currentTime = document.querySelector("#time");
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+function showTemp(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let displayedTemp = document.querySelector("#weather-now");
+  displayedTemp.innerHTML = `${temperature}°`;
+}
+function search(city) {
+  let units = "imperial";
+  let myApi = "1abc917551b1a4a6a106d16dc2865cf5";
+  let address = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${address}?q=${city}&appid=${myApi}&units=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
+function citySearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearch);
+
+search("Austin");
