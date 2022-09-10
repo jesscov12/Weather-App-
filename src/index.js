a4961096afd3f658eb545d1dcecd8500a3ab312f
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2022-09-10 12:36:02 -0500

    created a new function to get the forecast for the coordinates of the selected city
    Ran into API Key trouble

--- src/index.js
+++ src/index.js
@@ -57,6 +57,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+}
+
 function showTemp(response) {
   console.log(response);
   document.querySelector("#description").innerHTML =
@@ -75,6 +82,8 @@ function showTemp(response) {
   let temperature = Math.round(fahrenheitTemperature);
   let displayedTemp = document.querySelector("#weather-now");
   displayedTemp.innerHTML = `${temperature}°`;
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let units = "imperial";
