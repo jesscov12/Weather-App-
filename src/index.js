a8e5e3220b7807c54f2a980309ae1792c5a00868
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2022-09-13 11:47:41 -0500

    changed the open weather URL

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = `c95d60a1e3adbeb286133f1ebebc2579`;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
 }
 
