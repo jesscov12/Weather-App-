b90390c2e45db64f3118ca97233dacbc2ec8b4b3
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2022-09-14 20:24:50 -0500

    CSS updates to create better style

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ currentTime.innerHTML = `${hours}:${minutes}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
